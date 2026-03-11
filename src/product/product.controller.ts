436034426f16c7801fa2ba91d9aacf8a4f77e3c0
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2026-03-11 17:58:35 +0300

    update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductModel } from './product.model/product.model';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    @Post('create')
+    async create(@Body() dto: Omit<ProductModel, '_id'>){
+
+    }
+}
