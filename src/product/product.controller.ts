c04752ab3e6e05c2fbd54ef0258a3793c042d99e
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2026-03-12 15:48:24 +0300

    controllers

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { ProductModel } from './product.model/product.model';
+import { FindProductDto } from './dto/find-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -8,4 +9,26 @@ export class ProductController {
     async create(@Body() dto: Omit<ProductModel, '_id'>){
 
     }
+
+    @Get(':id')
+    async get(@Param('id') id: string) {
+
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+
+    }
+
+    @Patch(':id')
+    async patch(@Param('id') id: string, @Body() dto: ProductModel) {
+
+    }
+
+    @HttpCode(200)
+    @Post()
+    async find(@Body() dto: FindProductDto) {
+
+    }
+
 }
