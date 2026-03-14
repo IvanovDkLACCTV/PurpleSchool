8b21d6ce30f131e3486af6b06804f3b45ee66913
Dik131 <d.i.k.93@mail.ru>
2026-03-14 10:47:41 +0300

    resolved isssue with mongodb connection

--- mongo/readme.txt
+++ mongo/readme.txt
@@ -2,3 +2,4 @@ docker-compose up -d - поднять все контейнеры
 docker ps | grep mongo - посмотреть что запущено
 docker stop mongo - остановить контейнер
 docker start mongo - запустить контейнер
+docker-compose down - завершнить 
--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -1,21 +1,19 @@
+// mongo.config.ts
 import { ConfigService } from "@nestjs/config"
 import { MongooseModuleOptions } from "@nestjs/mongoose"
 
-export const getMongoConfig = async (ConfigService: ConfigService): Promise<MongooseModuleOptions> => {
+export const getMongoConfig = async (configService: ConfigService): Promise<MongooseModuleOptions> => {
     return {
-        uri: getMongoString(ConfigService),
-        ...getMongoOptions(),
+        uri: getMongoString(configService),
+        // больше ничего не нужно
     }
-  }
-
-  const getMongoString = (configService: ConfigService) => 
-    'mongodb://' + 
-    configService.get('MONGO_LOGIN') + ':' +
-    configService.get('MONGO_PASSWORD') + '@' +
-    configService.get('MONGO_HOST') + ':' +
-    configService.get('MONGO_PORT') + '/' +
-    configService.get('MONGO_AUTHDATABASE');
-
-  const getMongoOptions = () => ({
-    useNewUrlParser: true,
-  })
+}
+const getMongoString = (configService: ConfigService) => 
+  'mongodb://' + 
+  configService.get('MONGO_LOGIN') + ':' +
+  configService.get('MONGO_PASSWORD') + '@' +
+  configService.get('MONGO_HOST') + ':' +
+  configService.get('MONGO_PORT') + '/' +
+  configService.get('MONGO_DATABASE') +    // имя рабочей БД
+  '?authSource=' + 
+  configService.get('MONGO_AUTHDATABASE'); // admin
