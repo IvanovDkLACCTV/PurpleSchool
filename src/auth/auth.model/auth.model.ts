39963b776d819c06eab147f87837104025b70b4c
Dik131 <d.i.k.93@mail.ru>
2026-03-14 11:00:26 +0300

    use another syntaxis because I use nest mogooose instead of typegoose

--- src/auth/auth.model/auth.model.ts
+++ src/auth/auth.model/auth.model.ts
@@ -1,4 +1,13 @@
-export class AuthModel {
-    email: string
-    passwordHash: string
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ collection: 'Auth' })
+export class AuthModel extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModel, AuthSchema } from './auth.model/auth.model';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [MongooseModule.forFeature([
+    {
+      name: AuthModel.name,       // вместо mongooseClass
+      schema: AuthSchema,         // вместо schemaOptions
+    }
+  ])],
 })
 export class AuthModule {}
