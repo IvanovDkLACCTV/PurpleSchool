8efc285f5e0cdb98e6e14309b55c201efb9e14b4
Dik131 <d.i.k.93@mail.ru>
2026-03-17 16:57:53 +0300

    made unit test for review

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Document, Model, Types } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ReviewModel } from './review.model/review.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateReviewDto } from './dto/create-review.dto';
@@ -8,18 +8,18 @@ import { CreateReviewDto } from './dto/create-review.dto';
 export class ReviewService {
     constructor(@InjectModel(ReviewModel.name) private readonly reviewModel: Model<ReviewModel>) {}
 
-    async create(dto: CreateReviewDto): Promise<Document<ReviewModel>> {
-        return this.reviewModel.create(dto) as unknown as Document<ReviewModel>;
+    async create(dto: CreateReviewDto): Promise<ReviewModel> {
+        return this.reviewModel.create(dto);
     }
 
-    async delete(id: string): Promise<Document<ReviewModel> | null> {
-        return this.reviewModel.findByIdAndDelete(id).exec() as unknown as Document<ReviewModel> | null;
+    async delete(id: string): Promise<ReviewModel | null> {
+        return this.reviewModel.findByIdAndDelete(id).exec();
     }
 
-    async findByProductId(productId: string): Promise<Document<ReviewModel>[]> {
+    async findByProductId(productId: string): Promise<ReviewModel[]> {
         return this.reviewModel
             .find({ productId: new Types.ObjectId(productId) })
-            .exec() as unknown as Document<ReviewModel>[];
+            .exec();
     }
 
     async deleteByProductId(productId: string): Promise<{ deletedCount: number }> {
