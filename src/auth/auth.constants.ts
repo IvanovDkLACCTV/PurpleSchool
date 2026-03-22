998d25efd37a8c4a04318d9f2e217b265d145e8d
Dik131 <d.i.k.93@mail.ru>
2026-03-22 18:17:15 +0300

    wrote jwt strategy

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,4 @@
 export const ALREADY_REGISTRED_ERROR = 'User is already registred'
+export const USER_NOT_FOUND_ERROR = 'User is not found'
+export const WRONG_PASSWORD_ERROR = 'Wrong password'
+ 
--- src/auth/user.module.ts
+++ src/auth/user.module.ts
@@ -3,15 +3,25 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModel, UserSchema } from './auth.model/auth.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTAsync } from 'src/configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
-  imports: [MongooseModule.forFeature([
-    {
-      name: UserModel.name,       
-      schema: UserSchema,        
-    }
-  ])],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: UserModel.name,
+        schema: UserSchema,
+      }
+    ]),           
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTAsync
+    })
+  ],              
   providers: [AuthService],
 })
 export class AuthModule {}
