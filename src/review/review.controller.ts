e0cde663a5162b9abc863f636037a4992e2df6fd
Dik131 <d.i.k.93@mail.ru>
2026-03-23 12:39:35 +0300

    wrote user email decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -3,6 +3,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { AuthJwtGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -25,9 +26,10 @@ export class ReviewController {
         return deletedReview;
     }
 
-    
+    @UseGuards(AuthJwtGuard)
     @Get('byProduct/:productId')
-    async getByProduct(@Param('productId') productId: string) {
+    async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+        console.log(email)
         return this.reviewService.findByProductId(productId);
     }
 }
